2f54f6dced166019be20d51e006683309bdae627
Ivan Vasilenko <ivan.vasilenko@ys.bet>
2025-01-14 22:42:16 +0200

    Add part 2 scss lesson

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// let someImg = document.querySelector('.img');
+// let someBtn = document.querySelector('.btn');
+
+// someBtn.addEventListener('click', function() {someImg.setAttribute('src', '../img/log.png')});
+
+
+// -----------------------------------------
+
+// function Sum(num1, num2) {
+//     res = num1 + num2;
+//     console.log(res);
+// } 
+
+// Sum(2, 3);
+// Sum(50, 80);
+// Sum(100, 200);
+
+// ---------------------------------------------
+// function addInnerhtml(parentBlock, strHtml){
+//     parentBlock.innerHTML = strHtml;
+// };
+
+// addInnerhtml(megaphone, '<use xlink:href="assets/img/sprite/sprite.svg#megaphone-green"></use>');
+// addInnerhtml(building, '<use xlink:href="assets/img/sprite/sprite.svg#building-green"></use>');
