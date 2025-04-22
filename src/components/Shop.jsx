664e8c18867f6b044068d7bede3d2bdb01d63dfd
navicom1 <navicom1@navicom-nb.ys.aditsot.com>
2025-04-22 22:41:33 +0300

    Create new app react_shop

---
+++ src/components/Shop.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { API_KEY, API_URL } from "../config";
+import { Preloader } from "./Preloader";
+
+const Shop = () => {
+  const [goods, setGoods] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(function getGoods() {
+    fetch(API_URL, { 
+        headers: { 
+            'Authorization': API_KEY,
+        } })
+        .then(response => response.json())
+        .then(data => {
+            setGoods(data.items);
+            setLoading(false);
+        });
+  }, []);
+
+  return (
+    <main className="container content">
+      
+      {
+        loading ? <Preloader /> : goods
+      }
+    </main>
+  );
+};
+
+export { Shop };
